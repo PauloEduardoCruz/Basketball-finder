0e6babaa98f3f698851f7fc978bdbdee8579ede6
PauloEduardoCruz <pauloeduardo.crp@gmail.com>
2021-01-06 16:46:58 -0300

    Alteração das quatro páginas principais

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,19 +2,19 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import Landing from './pages/landing'
-import OrphanagesMap from './pages/OrphanagesMap'
-import Orphanage from './pages/Orphanage'
-import CreateOrphanage from './pages/CreateOrphanage'
+import EventsMap from './pages/EventsMap'
+import Event from './pages/Event'
+import CreateEvent from './pages/CreateEvent'
 
 function Routes() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Landing}></Route>
-                <Route path="/app" component={OrphanagesMap}></Route>
+                <Route path="/app" component={EventsMap}></Route>
 
-                <Route path="/event/create" component={CreateOrphanage}></Route>
-                <Route path="/event/:id" component={Orphanage}></Route>
+                <Route path="/event/create" component={CreateEvent}></Route>
+                <Route path="/event/:id" component={Event}></Route>
             </Switch>
         </BrowserRouter>
     )
